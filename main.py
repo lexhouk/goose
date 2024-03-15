f3a2cd2d016b69dfaab97d78e39dde6cdcc3a065
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-03-15 20:32:15 +0200

    Mentor's original code from the second day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from pygame.constants import QUIT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
+import random
 
 pygame.init()
 
@@ -10,6 +11,7 @@ WIDTH = 1200
 
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
+COLOR_BLUE = (0, 0, 255)
 
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -17,7 +19,23 @@ player_size = (20, 20)
 player = pygame.Surface(player_size)
 player.fill(COLOR_WHITE)
 player_rect = player.get_rect()
-player_speed = [1, 1]
+player_move_down = [0, 1]
+player_move_right = [1, 0]
+player_move_up = [0, -1]
+player_move_left = [-1, 0]
+
+def create_enemy():
+    enemy_size = (30, 30)
+    enemy = pygame.Surface(enemy_size)
+    enemy.fill(COLOR_BLUE)
+    enemy_rect = pygame.Rect(WIDTH, random.randint(0, HEIGHT), *enemy_size)
+    enemy_move = [random.randint(-6, -1), 0]
+    return [enemy, enemy_rect, enemy_move]
+
+CREATE_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_ENEMY, 1500)
+
+enemies = []
 
 playing = True
 
@@ -27,23 +45,27 @@ while playing:
     for event in pygame.event.get():
         if event.type == QUIT:
             playing = False
+        if event.type == CREATE_ENEMY:
+            enemies.append(create_enemy())
 
     main_display.fill(COLOR_BLACK)
 
-    main_display.blit(player, player_rect)
+    keys = pygame.key.get_pressed()
 
-    player_rect = player_rect.move(player_speed)
+    if keys[K_DOWN] and player_rect.bottom < HEIGHT:
+        player_rect = player_rect.move(player_move_down)
+
+    if keys[K_RIGHT] and player_rect.right < WIDTH:
+        player_rect = player_rect.move(player_move_right)
+
+    for enemy in enemies:
+        enemy[1] = enemy[1].move(enemy[2])
+        main_display.blit(enemy[0], enemy[1])
+
+    main_display.blit(player, player_rect)
 
     pygame.display.flip()
 
-    if player_rect.bottom >= HEIGHT:
-        player_speed[1] = -player_speed[1]
-
-    if player_rect.right >= WIDTH:
-        player_speed[0] = -player_speed[0]
-
-    if player_rect.top <= 0:
-        player_speed[1] = -player_speed[1]
-
-    if player_rect.left <= 0:
-        player_speed[0] = -player_speed[0]
+    for enemy in enemies:
+        if enemy[1].left < 0:
+            enemies.pop(enemies.index(enemy))
